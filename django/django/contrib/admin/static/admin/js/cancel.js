9fa8dc8623e573fd5c8600f7a147a4fa21950b9b
shadowson <wcook1613@gmail.com>
2018-01-31 22:01:46 -0500

    removed old django installation and updated project to use requirements.txt

--- django/django/contrib/admin/static/admin/js/cancel.js
+++
@@ -1,9 +0,0 @@
-(function($) {
-    'use strict';
-    $(function() {
-        $('.cancel-link').on('click', function(e) {
-            e.preventDefault();
-            window.history.back();
-        });
-    });
-})(django.jQuery);
